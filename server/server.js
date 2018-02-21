5c1e95c204143bd07a72c9a79a6f5dfcd84fff85
Zack Stout <zackstout@gmail.com>
2018-02-21 14:01:06 -0600

    great, we are getting a list of the stars in each constellation

--- server/server.js
+++ server/server.js
@@ -9,20 +9,38 @@ var constellations = [];
 
 var query = "Lists of stars by constellation";
 var options = {query: query, format: "html", summaryOnly: false};
-wikipedia.searchArticle(options, function(err, htmlWikiText){
+wikipedia.searchArticle(options, function(err, htmlWikiText) {
   if(err){
     console.log("An error occurred[query=%s, error=%s]", query, err);
     return;
   }
   var list = htmlWikiText.slice(htmlWikiText.indexOf('<ul>'), htmlWikiText.indexOf('</ul>'));
-  // console.log(list);
+  console.log(list);
   var arr = list.split("<li>");
   // console.log(arr);
   arr.forEach(function(item) {
     var conPrep = item.slice(1);
-    var constellation = item.slice(conPrep.indexOf('>') + 2, conPrep.indexOf('<'));
-    console.log(constellation);
+    var constellation = item.slice(conPrep.indexOf('>') + 2, conPrep.indexOf('<') + 1);
+    // console.log(constellation);
     constellations.push(constellation);
+
+  });
+
+  console.log(constellations);
+  constellations.forEach(function(con) {
+    console.log(con);
+    var query2 = "List of stars in " + con;
+    // var query2 = "List of stars in Virgo";
+    var options2 = {query: query2, format: "html", summaryOnly: true};
+    wikipedia.searchArticle(options2, function (err, html) {
+      if (err) {
+        console.log(err);
+       return;
+     } else {
+      console.log("Const data: ", html);
+     }
+   });
+
   });
 });
 
