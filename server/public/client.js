816bddf9300500bbc89a8e531a0520e6e64b7ca7
Zack Stout <zackstout@gmail.com>
2018-02-22 16:47:27 -0600

    wow, finally managed to get rid of those pesky NaNs

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,8 @@
 
+var orderedStars = [];
+
 $(document).ready(function() {
+  var allStars = new Array();
 
   // $.ajax({
   //   type: 'GET',
@@ -12,8 +15,8 @@ $(document).ready(function() {
   // });
 
   d3.csv('allthestars.csv', function(data) {
-    console.log(data);
-    console.log('======', '−' === '-');
+    // console.log(data);
+    // console.log('======', '−' === '-');
     data.forEach(function(star) {
 
       star.vismag = Number(star.vismag);
@@ -24,16 +27,70 @@ $(document).ready(function() {
         star.distance = Number(star.distance);
       }
 
-      // console.log('>' + star.absmag.charCodeAt(0) + '<');
-
       if (star.absmag.charCodeAt(0) === 8722 || star.absmag[0] === '-') {
         // console.log('huzzah');
         star.absmag = - parseFloat(star.absmag.slice(1));
+      } else {
+        star.absmag = parseFloat(star.absmag);
       }
-      star.absmag = parseFloat(star.absmag);
-
+      allStars.push(star);
+      // console.log(allStars);
     });
+
     console.log(data);
+    console.log(reorder(data));
+
+    // won't be able to access globally because of asynchronicity:
+    // allStars = data;
   });
 
 });
+
+function reorder(arr) {
+  var stars = arr;
+  var mags = [];
+  var orderedMags = [];
+  var newStars = [];
+  arr.forEach(function(star) {
+    mags.push(star.absmag);
+  });
+  console.log(mags);
+
+  //this is weird but whatever:
+  for (var i=0; i < mags.length; i++) {
+    var min = Math.min(mags);
+    if (isNaN(min)) {
+      var index = mags.findIndex(elem => isNaN(elem));
+      console.log(index);
+      if (index > 0) {
+        mags.splice(index, 1);
+      }
+    }
+  }
+
+  console.log(mags);
+
+  // while (isNaN(Math.min(mags))) {
+  //   var index = mags.findIndex(elem => isNaN(elem));
+  //   mags.splice(index, 1);
+  // }
+  //
+  // console.log(mags);
+  //
+  // while (mags.length > 0) {
+  //   var min = Math.min(mags);
+  //   var index;
+  //   // Oh, the problem is that indexOf cannot detect presence of NaN -- always returns -1:
+  //   if (isNaN(min)) {
+  //     index = mags.findIndex(elem => isNaN(elem));
+  //     console.log(index);
+  //     mags.splice(index, 1);
+  //   }
+  //
+  //   // orderedMags.push(min);
+  // }
+
+  console.log(orderedMags);
+
+
+}
